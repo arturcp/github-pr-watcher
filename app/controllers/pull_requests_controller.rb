68d37dc1daf9534b17d49de4345ffdb3ecee3d6a
Artur <artur.prado@gmail.com>
2019-08-05 11:52:42 -0300

    Let the API receive the authors list

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,11 +1,19 @@
 class PullRequestsController < ApplicationController
   def index
-    render json: client.open_pull_requests
+    if authors.present?
+      render json: client.open_pull_requests(authors)
+    else
+      head :unprocessable_entity
+    end
   end
 
   private
 
   def client
     @client ||= Github::Client.new
+  end
+
+  def authors
+    params[:authors] || []
   end
 end
--- app/models/github/client.rb
+++ app/models/github/client.rb
@@ -3,9 +3,9 @@ module Github
     API = 'https://api.github.com'
     AUTHORS = %w[arturcp betocattani caduaraujo felipecvo joaoscheuermann].freeze
 
-    def open_pull_requests
+    def open_pull_requests(authors)
       response = connection.get do |req|
-        req.url search_endpoint
+        req.url search_endpoint(build_list_for(authors.delete(' ')))
         req.headers['Accept'] = 'application/vnd.github.v3+json'
         req.headers['Authorization'] = "token #{ENV['TOKEN']}"
       end
@@ -24,12 +24,12 @@ module Github
       end
     end
 
-    def search_endpoint
+    def search_endpoint(authors)
       "/search/issues?q=#{authors}+state:open+org:youse-seguradora+is:pr"
     end
 
-    def authors
-      AUTHORS.map do |author|
+    def build_list_for(authors)
+      authors.split(',').map do |author|
         "author:#{author}"
       end.join('+')
     end
