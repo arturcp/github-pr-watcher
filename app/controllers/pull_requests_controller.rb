6a4befd86b3baaf3ca5abb8254b87998d724a501
Artur Caliendo Prado <artur.prado@gmail.com>
2022-08-17 16:43:42 -0300

    Upgrade ruby and rails versions (#2)
    * Upgrade ruby and rails versions
    
    * Remove database.yml

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,6 +1,4 @@
 class PullRequestsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def index
     if authors.present?
       render json: client.open_pull_requests
