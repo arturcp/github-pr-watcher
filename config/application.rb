e402c5b5f7fe36c4ac61a531d0d31477f86aabae
Artur Caliendo Prado <artur.prado@gmail.com>
2025-05-17 12:48:41 -0300

    Remove unused rails features (#49)
    * Remove unused rails features
    
    * Undo api only change
    
    * Delete test folder
    
    * Fix rspec
    
    * Remove dummy secret from dockerfile
    
    * Remove active_job reference
    
    * Update Dockerfile
    
    * Remove Dockerfile and switch to buildpacks
    
    * Remove action cable
    
    * Remove action job
    
    * Remove action mailer
    
    * Remove references to active record
    
    * Cleanup gemfile lock

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require_relative "boot"
 
-require "rails/all"
+# Only require the Rails components that you need
+require "rails"
+
+# Core Rails components
+require "action_controller/railtie"
+require "action_view/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,12 +16,13 @@ module GithubPrWatcher
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    # API-only mode
+    config.api_only = false
+
+    # Autoload paths
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
+    # Application configuration
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
