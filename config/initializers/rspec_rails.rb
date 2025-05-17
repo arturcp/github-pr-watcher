9c6b8c8bee0841e6a506a62c9cae6d9159428540
Artur Prado <artur.prado@gmail.com>
2025-05-17 11:59:36 -0300

    Delete test folder

---
+++ config/initializers/rspec_rails.rb
@@ -0,0 +1,17 @@
+# Disable previews in test environment
+if Rails.env.test?
+  Rails.application.config.action_controller.show_previews = false
+
+  # Disable view rendering in tests
+  config = Rails.application.config
+  config.action_controller.cache_store = :null_store
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+
+  # Disable asset compilation in tests
+  config.assets.compile = false
+  config.assets.debug = false
+
+  # Disable view rendering in specs
+  config.view_component.render_monkey_patch_enabled = false if defined?(ViewComponent)
+end
