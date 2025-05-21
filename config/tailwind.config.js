e6dd0d38bddb0e10a95b4660b35abd74a100c9e4
Artur Prado <artur.prado@gmail.com>
2025-05-20 21:30:09 -0300

    Fix deploy issues

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    "./public/*.html",
+    "./app/views/**/*.{erb,haml,html,slim}",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/assets/stylesheets/**/*.css"
   ],
   theme: {
     extend: {
